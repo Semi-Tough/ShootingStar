f45ec64381be97777aa76bfb90f48ee8724db3f6
Semi-Tough <1693416984@qq.com>
2022-04-11 13:44:21 +0800

    对象池的创建

---
+++ Assets/Scripts/SystemModules/PoolSystem/Pool.cs
@@ -0,0 +1,93 @@
+﻿/****************************************************
+    文件：Pool.cs
+    作者：wzq
+    邮箱：1693416984@qq.com
+    日期：2022/04/11 12:47:07
+    功能：对象池
+*****************************************************/
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[Serializable]
+public class Pool
+{
+    public GameObject Prefab => prefab;
+
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int size = 1;
+
+    private Queue<GameObject> queue;
+    private Transform parent;
+
+    public void Initialize(Transform poolParent)
+    {
+        queue = new Queue<GameObject>();
+        for (int i = 0; i < size; i++)
+        {
+            parent = poolParent;
+            queue.Enqueue(Copy());
+        }
+    }
+
+    private GameObject Copy()
+    {
+        GameObject go = Object.Instantiate(prefab, parent);
+        go.SetActive(false);
+        return go;
+    }
+
+    private GameObject AvailableGameObject()
+    {
+        GameObject availableGameObject;
+        if (queue.Count > 0 && !queue.Peek().activeSelf)
+        {
+            availableGameObject = queue.Dequeue();
+        }
+        else
+        {
+            availableGameObject = Copy();
+            queue.Enqueue(availableGameObject);
+        }
+
+        queue.Enqueue(availableGameObject);
+
+        return availableGameObject;
+    }
+
+    public GameObject PreparedObject()
+    {
+        GameObject preparedObject = AvailableGameObject();
+        preparedObject.SetActive(true);
+        return preparedObject;
+    }
+
+    public GameObject PreparedObject(Vector3 position)
+    {
+        GameObject preparedObject = AvailableGameObject();
+        preparedObject.transform.position = position;
+        preparedObject.SetActive(true);
+        return preparedObject;
+    }
+
+    public GameObject PreparedObject(Vector3 position, Quaternion rotation)
+    {
+        GameObject preparedObject = AvailableGameObject();
+        preparedObject.transform.position = position;
+        preparedObject.transform.rotation = rotation;
+        preparedObject.SetActive(true);
+        return preparedObject;
+    }
+
+    public GameObject PreparedObject(Vector3 position, Quaternion rotation, Vector3 localScale)
+    {
+        GameObject preparedObject = AvailableGameObject();
+        preparedObject.transform.position = position;
+        preparedObject.transform.rotation = rotation;
+        preparedObject.transform.localScale = localScale;
+        preparedObject.SetActive(true);
+        return preparedObject;
+    }
+}
---
+++ Assets/Scripts/SystemModules/PoolSystem/PoolManager.cs
@@ -0,0 +1,28 @@
+﻿/****************************************************
+    文件：PoolManager.cs
+    作者：wzq
+    邮箱：1693416984@qq.com
+    日期：2022/04/11 13:04:05
+    功能：对象池管理器
+*****************************************************/
+
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    [SerializeField] private Pool[] playerProjectilePools;
+
+    private void Start()
+    {
+        Initialized(playerProjectilePools);
+    }
+
+    private void Initialized(Pool[] pools)
+    {
+        foreach (Pool pool in pools)
+        {
+            Transform poolParent = new GameObject("Pool:" + pool.Prefab.name).transform;
+            pool.Initialize(poolParent);
+        }
+    }
+}
